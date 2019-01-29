174c0ead96d7d91764b2df134bed04e2030175e7
jmutt78 <jmcintosh@docusites.com>
2019-01-29 13:19:44 -0800

    -added ideas

--- src/components/newIdeaModal.js
+++ src/components/newIdeaModal.js
@@ -1,6 +1,30 @@
 import React from "react";
+import { connect } from "react-redux";
 
 class NewIdeaModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: true
+    };
+
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    const title = this.titleInput.value.trim();
+    if (title && this.props.onAdd) {
+      this.props.onAdd(this.titleInput.value);
+    }
+    this.titleInput.value = "";
+  }
+
+  setEditing(editing) {
+    this.setState({
+      editing
+    });
+  }
   render() {
     const { closeModal } = this.props;
     return (
@@ -16,9 +40,9 @@ class NewIdeaModal extends React.Component {
           </button>
           <h2 className="modal-title">Business Idea</h2>
           <div className="idea-body" />
-          <form>
+          <h3 className="title">Enter Your Business Idea</h3>
+          <form className="add-form" onSubmit={this.onSubmit}>
             <div className="form-group" role="form">
-              <h3 className="title">Enter Your Business Idea</h3>
               <textarea className="idea-title" rows="1" cols="50" />
               <h3 className="description">Write a brief description</h3>
               <textarea className="description-textarea" rows="5 " cols="100" />
@@ -36,4 +60,7 @@ class NewIdeaModal extends React.Component {
   }
 }
 
-export default NewIdeaModal;
+const mapStateToProps = state => ({
+  ideas: state.ideas
+});
+export default connect(mapStateToProps)(NewIdeaModal);
